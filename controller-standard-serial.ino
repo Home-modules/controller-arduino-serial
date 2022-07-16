a779f8d697ae66577710afa8305b37b53c8fc4c3
Mammad900 <mohammadhosseinabedi052@gmail.com>
2022-07-16 12:51:02 +0430

    Add DHT sensor support

--- controller-standard-serial.ino
+++ controller-standard-serial.ino
@@ -3,6 +3,12 @@
 #define CMD_DIGITAL_READ 2
 #define CMD_ANALOG_WRITE 3
 #define CMD_ANALOG_READ 4
+#define CMD_DHT11 50
+#define CMD_DHT21 51
+#define CMD_DHT22 52
+
+#include <dht.h>
+dht DHT;
 
 void setup() {
     Serial.begin(9600);
@@ -36,7 +42,7 @@ void loop() {
             byte id= Serial.read(); // ID helps the hub match the command and its response.
             Serial.write(id);
             Serial.write(digitalRead(pin));
-            byte value= Serial.read();
+            Serial.println();
             return;
         }
 
@@ -44,7 +50,39 @@ void loop() {
             byte id= Serial.read();
             Serial.write(id);
             Serial.write(analogRead(pin));
+            Serial.println();
             return;
+        }
+
+        if(command==CMD_DHT11) {
+            byte id= Serial.read();
+            Serial.write(id);
+            DHT.read11(pin);
+            float temp = DHT.temperature; // Convert to float (32-bit), because on Arduino Due a double is 64-bit.
+            float humi = DHT.humidity;
+            Serial.write((byte *) &temp, 4);
+            Serial.write((byte *) &humi, 4);
+            Serial.println();
+        }
+        if(command==CMD_DHT21) {
+            byte id= Serial.read();
+            Serial.write(id);
+            DHT.read21(pin);
+            float temp = DHT.temperature; // Convert to float (32-bit), because on Arduino Due a double is 64-bit.
+            float humi = DHT.humidity;
+            Serial.write((byte *) &temp, 4);
+            Serial.write((byte *) &humi, 4);
+            Serial.println();
+        }
+        if(command==CMD_DHT22) {
+            byte id= Serial.read();
+            Serial.write(id);
+            DHT.read22(pin);
+            float temp = DHT.temperature; // Convert to float (32-bit), because on Arduino Due a double is 64-bit.
+            float humi = DHT.humidity;
+            Serial.write((byte *) &temp, 4);
+            Serial.write((byte *) &humi, 4);
+            Serial.println();
         }
     }
 }
